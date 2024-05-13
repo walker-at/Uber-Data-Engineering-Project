dc84a52920bacfcf0e9b7b22f00d1410f118af8b
Walker Atterbury <161479815+walker-at@users.noreply.github.com>
2024-05-13 12:48:29 -0400

    Update extract-data.py

--- mage-scripts/extract-data.py
+++ mage-scripts/extract-data.py
@@ -1,1 +1,185 @@
 
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = 'https://storage.googleapis.com/walker-project-bucket/uber_data.csv'
+    response = requests.get(url)
+
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
+
+import pandas as pd
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(df, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your transformation logic here
+    df = df.drop_duplicates().reset_index(drop=True)
+    df['trip_id'] = df.index
+
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+
+    datetime_dim = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].reset_index(drop=True)
+    datetime_dim['tpep_pickup_datetime'] = datetime_dim['tpep_pickup_datetime']
+    datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
+    datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.day
+    datetime_dim['pick_month'] = datetime_dim['tpep_pickup_datetime'].dt.month
+    datetime_dim['pick_year'] = datetime_dim['tpep_pickup_datetime'].dt.year
+    datetime_dim['pick_weekday'] = datetime_dim['tpep_pickup_datetime'].dt.weekday
+
+    datetime_dim['tpep_dropoff_datetime'] = datetime_dim['tpep_dropoff_datetime']
+    datetime_dim['drop_hour'] = datetime_dim['tpep_dropoff_datetime'].dt.hour
+    datetime_dim['drop_day'] = datetime_dim['tpep_dropoff_datetime'].dt.day
+    datetime_dim['drop_month'] = datetime_dim['tpep_dropoff_datetime'].dt.month
+    datetime_dim['drop_year'] = datetime_dim['tpep_dropoff_datetime'].dt.year
+    datetime_dim['drop_weekday'] = datetime_dim['tpep_dropoff_datetime'].dt.weekday
+
+    datetime_dim['datetime_id'] = datetime_dim.index
+    datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
+                                'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
+
+    passenger_count_dim = df[['passenger_count']].reset_index(drop=True)
+    passenger_count_dim['passenger_count_id'] = passenger_count_dim.index # assign index
+    passenger_count_dim = passenger_count_dim[['passenger_count_id','passenger_count']] # reorder columns
+
+    trip_distance_dim = df[['trip_distance']].reset_index(drop=True)
+    trip_distance_dim['trip_distance_id'] = trip_distance_dim.index # assign index
+    trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']] # reorder columns
+    
+    rate_code_type = {
+        1:"Standard rate",
+        2:"JFK",
+        3:"Newark",
+        4:"Nassau or Westchester",
+        5:"Negotiated fare",
+        6:"Group ride"
+    }
+
+    rate_code_dim = df[['RatecodeID']].reset_index(drop=True)
+    rate_code_dim['rate_code_id'] = rate_code_dim.index # assign index
+    rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type) # map name values to RatecodeID to get our new column
+    rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']] # reorder columns
+
+    pickup_location_dim = df[['pickup_longitude', 'pickup_latitude']].reset_index(drop=True)
+    pickup_location_dim['pickup_location_id'] = pickup_location_dim.index # assign index
+    pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_latitude','pickup_longitude']] # reorder columns
+
+    dropoff_location_dim = df[['dropoff_longitude', 'dropoff_latitude']].reset_index(drop=True)
+    dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index # assign index
+    dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_latitude','dropoff_longitude']] # reorder columns
+
+    payment_type_name = {
+        1:"Credit card",
+        2:"Cash",
+        3:"No charge",
+        4:"Dispute",
+        5:"Unknown",
+        6:"Voided trip"
+    }
+
+    payment_type_dim = df[['payment_type']].reset_index(drop=True)
+    payment_type_dim['payment_type_id'] = payment_type_dim.index # assign index
+    payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name) # map values
+    payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']] # reorder columns
+
+    fact_table = df.merge(passenger_count_dim, left_on='trip_id', right_on='passenger_count_id') \
+             .merge(trip_distance_dim, left_on='trip_id', right_on='trip_distance_id') \
+             .merge(rate_code_dim, left_on='trip_id', right_on='rate_code_id') \
+             .merge(pickup_location_dim, left_on='trip_id', right_on='pickup_location_id') \
+             .merge(dropoff_location_dim, left_on='trip_id', right_on='dropoff_location_id')\
+             .merge(datetime_dim, left_on='trip_id', right_on='datetime_id') \
+             .merge(payment_type_dim, left_on='trip_id', right_on='payment_type_id') \
+             [['trip_id','VendorID', 'datetime_id', 'passenger_count_id',
+               'trip_distance_id', 'rate_code_id', 'store_and_fwd_flag', 'pickup_location_id', 'dropoff_location_id',
+               'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
+               'improvement_surcharge', 'total_amount']]
+
+    
+    return {"datetime_dim":datetime_dim.to_dict(orient="dict"),
+    "passenger_count_dim":passenger_count_dim.to_dict(orient="dict"),
+    "trip_distance_dim":trip_distance_dim.to_dict(orient="dict"),
+    "rate_code_dim":rate_code_dim.to_dict(orient="dict"),
+    "pickup_location_dim":pickup_location_dim.to_dict(orient="dict"),
+    "dropoff_location_dim":dropoff_location_dim.to_dict(orient="dict"),
+    "payment_type_dim":payment_type_dim.to_dict(orient="dict"),
+    "fact_table":fact_table.to_dict(orient="dict")}
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
+
+
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.snowflake import Snowflake
+from pandas import DataFrame
+from os import path
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_snowflake(data, **kwargs) -> None:
+    """
+    Template for exporting data to a Snowflake warehouse.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#snowflake
+    """
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    for key, value in data.items():
+        table_name = '{}'.format(key)
+        database = 'WALKER_DE_PROJECT'
+        schema = 'RAW'
+        with Snowflake.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
+            loader.export(
+                DataFrame(value),
+                table_name,
+                database,
+                schema,
+                if_exists='replace',  # Specify resolution policy if table already exists
+            )
+
